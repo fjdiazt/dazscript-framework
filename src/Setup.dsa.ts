85c4b2ce9825d57f5e915bc7aaa2ad0369dd3824
Freddy Diaz <fjdiazt@gmail.com>
2026-04-23 13:07:07 -0400

    feat: add additional sample scripts for Hello World, App Settings, Local Files, Simple Dialog, Settings Dialog, and List Dialog

--- src/Setup.dsa.ts
+++ src/Setup.dsa.ts
@@ -4,6 +4,48 @@ import { showSetupCustomActionsDialog as setup } from '@dsf/helpers/custom-actio
 setup([
     {
         "name": null,
+        "text": "01 Hello World",
+        "filePath": "samples/01-hello-world.dsa",
+        "menuPath": "/DazScriptFramework/samples",
+        "description": "01-hello-world"
+    },
+    {
+        "name": null,
+        "text": "02 App Settings",
+        "filePath": "samples/02-app-settings.dsa",
+        "menuPath": "/DazScriptFramework/samples",
+        "description": "02-app-settings"
+    },
+    {
+        "name": null,
+        "text": "03 Local Files",
+        "filePath": "samples/03-local-files.dsa",
+        "menuPath": "/DazScriptFramework/samples",
+        "description": "03-local-files"
+    },
+    {
+        "name": null,
+        "text": "04 Simple Dialog",
+        "filePath": "samples/04-simple-dialog.dsa",
+        "menuPath": "/DazScriptFramework/samples",
+        "description": "04-simple-dialog"
+    },
+    {
+        "name": null,
+        "text": "05 Settings Dialog",
+        "filePath": "samples/05-settings-dialog.dsa",
+        "menuPath": "/DazScriptFramework/samples",
+        "description": "05-settings-dialog"
+    },
+    {
+        "name": null,
+        "text": "06 List Dialog",
+        "filePath": "samples/06-list-dialog.dsa",
+        "menuPath": "/DazScriptFramework/samples",
+        "description": "06-list-dialog"
+    },
+    {
+        "name": null,
         "text": "Hello World",
         "filePath": "samples/hello-world.dsa",
         "menuPath": "/DazScriptFramework/samples",
